9498a2589c5efd280fddeddcc17cd74d33a283d1
ekeycept <gorbonosdd@mail.ru>
2022-11-20 13:51:57 +0300

    Add animations
    Idle, Run, Jump, Shooting, GetDamage

--- Pub Duels/Assets/Scripts/Player.cs
+++ Pub Duels/Assets/Scripts/Player.cs
@@ -11,6 +11,9 @@ public class Player : MonoBehaviour, IDamageable
     private float directionMove;
     [SerializeField] private float speed = 40f;
     private bool jump = false;
+    [SerializeField] private Animator animator;
+    private bool isRunning = false;
+    public SpriteRenderer sr;
 
     [SerializeField] private FieldOfView fieldOfView;
 
@@ -18,6 +21,7 @@ public class Player : MonoBehaviour, IDamageable
     {
         Cursor.visible = false;
         controller = GetComponent<CharacterController2D>();
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
     }
     
     void Update()
@@ -28,21 +32,43 @@ public class Player : MonoBehaviour, IDamageable
 
         directionMove = Input.GetAxisRaw("Horizontal") * speed;
 
+        if (directionMove != 0)
+            animator.SetBool("isRunning", true);
+        else
+            animator.SetBool("isRunning", false);
+
         if (Input.GetButtonDown("Jump"))
+        {
             jump = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+            GetDamage(2);
     }
 
     private void FixedUpdate()
     {
         controller.Move(directionMove * Time.deltaTime, false, jump);
         jump = false;
+        animator.SetBool("isJumping", false);
     }
 
     public void GetDamage(int damage)
     {
         health -= damage;
+        StartCoroutine(ColorChange());
 
         if (health <= 0)
             Destroy(gameObject);
-    }    
+    }
+
+    IEnumerator ColorChange()
+    {
+        sr.color = Color.red;
+        yield return new WaitForSeconds(.7f);
+        sr.color = Color.white;
+    }
+    
 }
+
